2d2d012d3427cf7d37d9d3335c2a5f00931e2a6e
Irwin <irgranados16@gmail.com>
2020-11-17 19:11:49 -0600

    set up deployment

---
+++ client/build/precache-manifest.1bc91ecf3683e5e98dd31ff9ab223f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c6a892833baecbfa5d23033cf347d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ecec69425695a6eaca3",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "5309e449abb858676493",
+    "url": "/static/js/2.7f4868fe.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.7f4868fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecec69425695a6eaca3",
+    "url": "/static/js/main.1f7e7cb6.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
